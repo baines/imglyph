3848d1e89ef0bf80698d198e8077b06db48adb59
Alex Baines <alex@abaines.me.uk>
2018-03-25 16:24:44 +0100

    add colour mode (#1)

--- main.c
+++ main.c
@@ -7,13 +7,26 @@
 #include <inttypes.h>
 #include <wchar.h>
 #include <locale.h>
+#include <float.h>
+
+/* config: */
 
 //#define IRC_MODE
 #define HALF_RES
 #define DO_DITHER
+#define COLOUR_MODE
+
+/* end config */
 
 #define countof(x) (sizeof(x)/sizeof(*x))
+#define MAX(a, b)  ((a)>(b)?(a):(b))
+#define MIN(a, b)  ((a)<=(b)?(a):(b))
 
+typedef uint8_t v4b __attribute__((vector_size(4)));
+typedef uint8_t v8b __attribute__((vector_size(8)));
+typedef float   v4  __attribute__((vector_size(16)));
+
+/*
 static const uint8_t dither[8][8] = {
 	{ 0 , 48, 12, 60, 3 , 51, 15, 63 },
 	{ 32, 16, 44, 28, 35, 19, 47, 31 },
@@ -24,6 +37,7 @@ static const uint8_t dither[8][8] = {
 	{ 10, 58, 6 , 54, 9 , 57, 5 , 53 },
 	{ 42, 26, 38, 22, 41, 25, 37, 21 },
 };
+*/
 
 // "braille dither"
 static const uint8_t dither2[8][8] = {
@@ -155,7 +169,7 @@ void gen_braille_glyphs(struct glyph* g){
 struct glyph* glyphs;
 size_t nglyphs;
 
-void print_nearest(uint64_t mask){
+struct glyph* get_nearest_glyph(uint64_t mask, bool* out_reverse){
 
 	int best_idx = 0;
 	int best_pop = INT_MAX;
@@ -186,8 +200,147 @@ void print_nearest(uint64_t mask){
 		}
 	}
 
-	char str[MB_LEN_MAX];
-	wctomb(str, glyphs[best_idx].code);
+	*out_reverse = reverse;
+	return glyphs + best_idx;
+}
+
+#ifdef COLOUR_MODE
+#ifdef IRC_MODE
+float colour_dist(uint32_t one, uint32_t two){
+	long r = abs((long)((one >> 16) & 0xFF) - (long)((two >> 16) & 0xFF));
+	long g = abs((long)((one >>  8) & 0xFF) - (long)((two >>  8) & 0xFF));
+	long b = abs((long)((one >>  0) & 0xFF) - (long)((two >>  0) & 0xFF));
+	return (r+g+b)/3.0f;
+}
+#endif // IRC_MODE
+
+void print_glyph_colour(uint32_t code, v4 fg, v4 bg){
+	char str[MB_LEN_MAX] = {};
+	wctomb(str, code);
+
+	uint8_t ansi_fg = 16
+		+ 36 * MIN(5, (int)(fg[0] * 6.0f))
+		+ 6  * MIN(5, (int)(fg[1] * 6.0f))
+		+      MIN(5, (int)(fg[2] * 6.0f));
+
+	uint8_t ansi_bg = 16
+		+ 36 * MIN(5, (int)(bg[0] * 6.0f))
+		+ 6  * MIN(5, (int)(bg[1] * 6.0f))
+		+      MIN(5, (int)(bg[2] * 6.0f));
+
+#ifdef IRC_MODE
+	static const uint32_t irc_colours[] = {
+		0xffffff, 0x000000, 0x00007f, 0x009300, 0xff0000, 0x7f0000, 0x9c009c, 0xfc7f00, 0xffff00, 0x00fc00, 0x009393, 0x00ffff,
+		0x0000fc, 0xff00ff, 0x7f7f7f, 0xd2d2d2,
+		0x470000, 0x472100, 0x474700, 0x324700, 0x004700, 0x00472c, 0x004747, 0x002747, 0x000047, 0x2e0047, 0x470047, 0x47002a,
+		0x740000, 0x743a00, 0x747400, 0x517400, 0x007400, 0x007449, 0x007474, 0x004074, 0x000074, 0x4b0074, 0x740074, 0x740045,
+		0xb50000, 0xb56300, 0xb5b500, 0x7db500, 0x00b500, 0x00b571, 0x00b5b5, 0x0063b5, 0x0000b5, 0x7500b5, 0xb500b5, 0xb5006b,
+		0xff0000, 0xff8c00, 0xffff00, 0xb2ff00, 0x00ff00, 0x00ffa0, 0x00ffff, 0x008cff, 0x0000ff, 0xa500ff, 0xff00ff, 0xff0098,
+		0xff5959, 0xffb459, 0xffff71, 0xcfff60, 0x6fff6f, 0x65ffc9, 0x6dffff, 0x59b4ff, 0x5959ff, 0xc459ff, 0xff66ff, 0xff59bc,
+		0xff9c9c, 0xffd39c, 0xffff9c, 0xe2ff9c, 0x9cff9c, 0x9cffdb, 0x9cffff, 0x9cd3ff, 0x9c9cff, 0xdc9cff, 0xff9cff, 0xff94d3,
+		0x000000, 0x131313, 0x282828, 0x363636, 0x4d4d4d, 0x656565, 0x818181, 0x9f9f9f, 0xbcbcbc, 0xe2e2e2, 0xffffff,
+	};
+
+	static const uint8_t irc_map[256] = {
+		[ 52]=16, [ 94]=17, [100]=18, [ 58]=19, [ 22]=20, [ 29]=21, [ 23]=22, [ 24]=23, [ 17]=24, [ 54]=25, [ 53]=26, [ 89]=27,
+		[ 88]=28, [130]=29, [142]=30, [ 64]=31, [ 28]=32, [ 35]=33, [ 30]=34, [ 25]=35, [ 18]=36, [ 91]=37, [ 90]=38, [125]=39,
+		[124]=40, [166]=41, [184]=42, [106]=43, [ 34]=44, [ 49]=45, [ 37]=46, [ 33]=47, [ 19]=48, [129]=49, [127]=50, [161]=51,
+		[196]=52, [208]=53, [226]=54, [154]=55, [ 46]=56, [ 86]=57, [ 51]=58, [ 75]=59, [ 21]=60, [171]=61, [201]=62, [198]=63,
+		[203]=64, [215]=65, [227]=66, [191]=67, [ 83]=68, [122]=69, [ 87]=70, [111]=71, [ 63]=72, [177]=73, [207]=74, [205]=75,
+		[217]=76, [223]=77, [229]=78, [193]=79, [157]=80, [158]=81, [159]=82, [153]=83, [147]=84, [183]=85, [219]=86, [212]=86,
+	};
+
+	int i_fg = irc_map[ansi_fg], i_bg = irc_map[ansi_bg];
+
+	if(i_fg == 0){
+		uint8_t a_r = MIN(5, (int)(fg[0] * 6.0f));
+		uint8_t a_g = MIN(5, (int)(fg[1] * 6.0f));
+		uint8_t a_b = MIN(5, (int)(fg[2] * 6.0f));
+
+		static const int fudge[] = { 0, +1, -1 };
+
+		for(int a = 0; a < 3; ++a){
+			for(int b = 0; b < 3; ++b){
+				for(int c = 0; c < 3; ++ c){
+					uint8_t new_ansi = 16
+						+ 36 * (a_r + fudge[a])
+						+  6 * (a_g + fudge[b])
+						+      (a_b + fudge[c]);
+
+					if(irc_map[new_ansi] != 0){
+						i_fg = irc_map[new_ansi];
+						goto done_fg;
+					}
+
+				}
+			}
+		}
+	}
+done_fg:;
+	if(i_bg == 0){
+		uint8_t a_r = MIN(5, (int)(bg[0] * 6.0f));
+		uint8_t a_g = MIN(5, (int)(bg[1] * 6.0f));
+		uint8_t a_b = MIN(5, (int)(bg[2] * 6.0f));
+
+		static const int fudge[] = { 0, -1, +1 };
+
+		for(int a = 0; a < 3; ++a){
+			for(int b = 0; b < 3; ++b){
+				for(int c = 0; c < 3; ++ c){
+					uint8_t new_ansi = 16
+						+ 36 * (a_r + fudge[a])
+						+  6 * (a_g + fudge[b])
+						+      (a_b + fudge[c]);
+
+					if(irc_map[new_ansi] != 0){
+						i_bg = irc_map[new_ansi];
+						goto done_bg;
+					}
+
+				}
+			}
+		}
+	}
+done_bg:;
+
+	float b_fg = i_fg ? 0 : FLT_MAX, b_bg = i_bg ? 0 : FLT_MAX;
+
+	uint32_t hex_fg = 
+		(MIN(255, (int)(fg[0] * 256.0f)) << 16) |
+		(MIN(255, (int)(fg[1] * 256.0f)) << 8) |
+		(MIN(255, (int)(fg[2] * 256.0f)));
+
+	uint32_t hex_bg = 
+		(MIN(255, (int)(bg[0] * 256.0f)) << 16) |
+		(MIN(255, (int)(bg[1] * 256.0f)) << 8) |
+		(MIN(255, (int)(bg[2] * 256.0f)));
+
+	for(int i = 0; i < countof(irc_colours); ++i){
+		uint32_t c = irc_colours[i];
+		float fg_diff = colour_dist(c, hex_fg);
+		float bg_diff = colour_dist(c, hex_bg);
+
+		if(fg_diff < b_fg){
+			b_fg = fg_diff;
+			i_fg = i;
+		}
+
+		if(bg_diff < b_bg){
+			b_bg = bg_diff;
+			i_bg = i;
+		}
+	}
+
+	printf("\003%d,%d%s", i_fg, i_bg, str);
+#else
+	printf("\e[38;5;%d;48;5;%dm%s\e[0m", ansi_fg, ansi_bg, str);
+#endif // IRC_MODE
+}
+#endif // COLOUR_MODE
+
+void print_glyph(uint32_t code, bool reverse){
+	char str[MB_LEN_MAX] = {};
+	wctomb(str, code);
 
 #ifdef IRC_MODE
 	if(reverse){
@@ -207,7 +360,7 @@ void print_nearest(uint64_t mask){
 int main(int argc, char** argv){
 
 	// hope you've got a utf-8 locale
-	assert(setlocale(LC_CTYPE, "C.UTF-8") || setlocale(LC_CTYPE, "en_US.utf8"));
+	assert(setlocale(LC_CTYPE, "C.UTF-8") || setlocale(LC_CTYPE, "en_US.utf8") || setlocale(LC_CTYPE, ""));
 
 	if(argc < 2){
 		fprintf(stderr, "Usage: %s [file]\n", argv[0]);
@@ -215,7 +368,7 @@ int main(int argc, char** argv){
 	}
 
 	nglyphs = countof(_glyphs) + 255;
-	glyphs = malloc(sizeof(_glyphs) + (255 * sizeof(struct glyph)));
+	glyphs = calloc(nglyphs, sizeof(struct glyph));
 	memcpy(glyphs, _glyphs, sizeof(_glyphs));
 	gen_braille_glyphs(glyphs + countof(_glyphs));
 
@@ -229,20 +382,31 @@ int main(int argc, char** argv){
 	//printf("w: %d, h: %d\n", w, h);
 
 	uint8_t (*in)[w*4] = img;
+
+#ifdef HALF_RES
+	for(int y = 0; y + 1 < h; y+=2){
+		for(int x = 0; x < w*4; ++x){
+			in[y/2][x] = (int)(in[y][x] + in[y+1][x]) / 2;
+		}
+	}
+
+	h /= 2;
+#endif
+
 	uint8_t (*out)[w*4] = malloc(w*h*4);
 	uint8_t gs_min = 255, gs_maw = 0;
 
-	for(int i = 0; i < h; ++i){
-		for(int j = 0; j < w*4; j+=4){
+	for(int y = 0; y < h; ++y){
+		for(int x = 0; x < w*4; x+=4){
 			float gs
-				= (in[i][j+0] / 255.0) * 0.2126
-				+ (in[i][j+1] / 255.0) * 0.7152
-				+ (in[i][j+2] / 255.0) * 0.0722;
+				= (in[y][x+0] / 255.0) * 0.2126
+				+ (in[y][x+1] / 255.0) * 0.7152
+				+ (in[y][x+2] / 255.0) * 0.0722;
 
 			uint8_t v = (gs*255);
 
-			memset(&out[i][j], v, 3);
-			out[i][j+3] = 255;
+			memset(&out[y][x], v, 3);
+			out[y][x+3] = 255;
 
 			if(v < gs_min) gs_min = v;
 			if(v > gs_maw) gs_maw = v;
@@ -251,38 +415,6 @@ int main(int argc, char** argv){
 
 	float scale = 255.0 / (float)(gs_maw - gs_min);
 
-#ifdef HALF_RES
-	uint8_t (*half)[w*4] = malloc(w*h*2);
-	for(int y = 0; y + 1 < h; y+=2){
-		for(int x = 0; x < w*4; x+=4){
-			uint32_t v
-				= (int)((out[y+0][x] - gs_min) * scale
-				+  (out[y+1][x] - gs_min) * scale) >> 1;
-
-#ifdef DO_DITHER
-			//if((v >> 2) > dither[(y/2)%8][(x/4)%8]){
-			v >>= 2;
-			uint8_t d = dither2[(y/2)%8][(x/4)%8];
-			if( (v >= 32 && (v - 32) >= d) ||
-				(v <= 31 && (31 - v) <= d)){
-#else
-			if(v >= 127){
-#endif
-				v = 255;
-			} else {
-				v = 0;
-			}
-
-			memset(&half[y/2][x], v, 3);
-			half[y/2][x+3] = 255;
-		}
-	}
-
-	free(out);
-	out = half;
-
-	h /= 2;
-#else
 	for(int y = 0; y < h; ++y){
 		for(int x = 0; x < w*4; x+=4){
 			uint32_t v = (int)((out[y][x] - gs_min) * scale);
@@ -305,11 +437,7 @@ int main(int argc, char** argv){
 		}
 	}
 
-#endif
-
 	//printf("min: %d, maw: %d\n", gs_min, gs_maw);
-
-	int cnt = 0;
 
 	for(int y = 0; y + 8 < h; y += 8){
 		for(int x = 0; x + 32 < (w*4); x += (8*4)){
@@ -323,8 +451,37 @@ int main(int argc, char** argv){
 				}
 			}
 
-			//fprintf(stderr, "mask %d = %#" PRIx64 "\n", cnt++, mask);
-			print_nearest(mask);
+			bool reverse;
+			struct glyph* g = get_nearest_glyph(mask, &reverse);
+
+#ifdef COLOUR_MODE
+			v4 fg = {}, bg = {};
+			int fgcnt = 0;
+
+			for(int i = 0; i < 8; ++i){
+				for(int j = 0; j < 8; ++j){
+					v8b c = *(v8b*)&in[y+i][x+j*4];
+					v4 color = (v4)_mm_cvtpu16_ps((__m64)__builtin_ia32_punpcklbw((__v8qi)c, (__v8qi){}));
+
+					if(g->mask & (UINT64_C(1) << (i*8+j))){
+						fg += color;
+						fgcnt++;
+					} else {
+						bg += color;
+					}
+				}
+			}
+
+			if(fgcnt) fg /= (float)fgcnt;
+			if(fgcnt != 64)	bg /= (64.0f - fgcnt);
+
+			fg /= 255.0f;
+			bg /= 255.0f;
+
+			print_glyph_colour(g->code, fg, bg);
+#else
+			print_glyph(g->code, reverse);
+#endif
 		}
 
 		puts("");
--- readme.md
+++ readme.md
@@ -4,7 +4,10 @@ hack to create rough representation of an image with unicode glyphs
 
 todo:
 
-+ colour
++ fix colour mode to use all 256 colors, not just the 216 middle ones
++ add true colour (24-bit) mode for terminal
++ add irc \004 true colour mode
++ dither colours
 + scaling
 + use unicode combining marks n stuff
 + less arbitrary choice of glyphs
